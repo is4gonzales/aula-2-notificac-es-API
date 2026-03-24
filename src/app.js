24b8269da1747ae0164ce93ef87174ea6ba7c342
is4gonzales <isabele.firmino@aluno.senai.br>
2026-03-24 10:32:12 -0300

    Middlewares

--- src/app.js
+++ src/app.js
@@ -1,28 +1,23 @@
 const express = require("express");
-const swaggerUi = require("swagger-ui-express");
-const swaggerSpec = require("./swagger");
+const cors = require("cors");
+
+const logger = require("./middlewares/logger");
+const notFound = require("./middlewares/notFound");
+
+const notificacoesRoutes = require("./routes/notificacoesRoutes");
+
 const app = express();
-// Middlewares
+
 app.use(express.json());
-// Documentação Swagger
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-// Rotas
-const eventoRoutes = require("./routes/eventoRoutes");
-const participanteRoutes = require("./routes/participanteRoutes");
-const inscricaoRoutes = require("./routes/inscricaoRoutes");
-app.use("/eventos", eventoRoutes);
-app.use("/participantes", participanteRoutes);
-app.use("/inscricoes", inscricaoRoutes);
-// Rota raiz
+app.use(cors());
+app.use(logger);
+
+app.use("/notificacoes", notificacoesRoutes);
+
 app.get("/", (req, res) => {
-  res.json({
-    mensagem: "API de Notificações",
-    documentacao: "/api-docs",
-    rotas: {
-      eventos: "/eventos",
-      participantes: "/participantes",
-      inscricoes: "/inscricoes",
-    },
-  });
+  res.json({ mensagem: "API de Notificações funcionando!" });
 });
+
+app.use(notFound);
+
 module.exports = app;
